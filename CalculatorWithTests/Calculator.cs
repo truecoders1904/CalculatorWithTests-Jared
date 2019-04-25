9cecda4732c5f1524d91856e507fbdfa79c7beee
Jared Clark <jmjclark79@gmail.com>
2019-04-25 16:06:07 -0500

    Added Calculator Class and five methods

---
+++ CalculatorWithTests/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorWithTests
+{
+    class Calculator
+    {
+        static int Add(List<int>numbersToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        static int Substract(List<int>numbersToSubstract)
+        {
+            throw new NotImplementedException();
+        }
+
+        static int Multiply(List<int> numbersToMultiply)
+        {
+            throw new NotImplementedException();
+        }
+
+        static int Divide(List<int> numbersToDivide)
+        {
+            throw new NotImplementedException();
+        }
+
+        static int Factoial(List<int> numbersToFactorialize)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
